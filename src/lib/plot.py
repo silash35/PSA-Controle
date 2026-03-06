8908f31a03da551157d78ec29d44a2db5be12763
Silas Henrique <silash35@gmail.com>
2026-03-05 22:50:07 -0300

    Split test/train

--- src/lib/plot.py
+++ src/lib/plot.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import colors as mcolors
 
-from .read_data import y_labels
-
 colors = list(mcolors.TABLEAU_COLORS.keys())
 
 plt.rcParams.update(
@@ -18,32 +16,3 @@ plt.rcParams.update(
 
 def legend_outside(ax: plt.Axes):
     ax.legend(loc="center left", bbox_to_anchor=(1, 0.5), frameon=False)
-
-
-def plot_comparison(cycles, real_y, model_y, model_name):
-    ymin = min(real_y[:, i].min() for i in range(len(y_labels)))
-    ymax = max(real_y[:, i].max() for i in range(len(y_labels)))
-
-    for i in range(len(y_labels)):
-        plt.scatter(
-            cycles,
-            real_y[:, i],
-            label=f"{y_labels[i]} (gPROMS)",
-            color=colors[i],
-            alpha=0.6,
-        )
-        plt.plot(
-            cycles,
-            model_y[:, i],
-            label=f"{y_labels[i]} ({model_name})",
-            color=colors[i],
-        )
-    plt.ylim(ymin * 0.9, ymax * 1.1)
-
-    legend_outside(plt.gca())
-    plt.ylabel("Valores / %")
-    plt.xlabel("Ciclo")
-
-    # plt.show()
-    plt.savefig(f"../figures/comparision-{model_name}.png")
-    plt.close()
